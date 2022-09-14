8955c1373e9994ec83972fe3f2767b17e65c248d
Lok Chan <0903lokchan@gmail.com>
2022-09-15 00:15:18 +1200

    add camera

--- code/sprites.py
+++ code/sprites.py
@@ -2,7 +2,8 @@ import pygame
 from settings import *
 
 class Generic(pygame.sprite.Sprite):
-    def __init__(self, pos, surf: pygame.surface.Surface, groups: pygame.sprite.AbstractGroup) -> None:
+    def __init__(self, pos, surf: pygame.surface.Surface, groups: pygame.sprite.AbstractGroup, z:int = LAYERS['main']) -> None:
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_rect(topleft = pos)
+        self.z = z
