3405ca2db9e9ec8af2f27dce80d836d975902e74
Lok Chan <0903lokchan@gmail.com>
2023-10-07 14:46:01 +1300

    implemented soil class

--- code/level.py
+++ code/level.py
@@ -6,6 +6,7 @@ from sprites import Generic, Interaction, Water, WildFlower, Tree
 from pytmx.util_pygame import load_pygame
 from support import *
 from transition import Transition
+from soil import SoilLayer
 
 class Level:
     def __init__(self) -> None:
@@ -19,6 +20,7 @@ class Level:
         self.tree_sprites = pygame.sprite.Group()
         self.interaction_sprites = pygame.sprite.Group()
 
+        self.soil_layer = SoilLayer(self.all_sprites)
         self.setup()
         self.overlay = Overlay(self.player)
         self.transition = Transition(self.reset, self.player)
@@ -68,7 +70,9 @@ class Level:
                     group = self.all_sprites, 
                     collision_sprites = self.collision_sprites, 
                     tree_sprites = self.tree_sprites,
-                    interaction = self.interaction_sprites)
+                    interaction = self.interaction_sprites,
+                    soil_layer = self.soil_layer
+                )
             if obj.name == 'Bed':
                 Interaction((obj.x, obj.y), (obj.width, obj.height), self.interaction_sprites, obj.name)
           
@@ -95,6 +99,7 @@ class Level:
         self.display_surface.fill('black')
         self.all_sprites.custom_draw(self.player)
         self.all_sprites.update(dt)
+        
 
         self.overlay.display()
         
