07d342ef879c901d44b7896f2b7cc526e9b19ef6
Lok Chan <0903lokchan@gmail.com>
2023-11-16 20:29:13 +1300

    Added rain update logic

--- code/level.py
+++ code/level.py
@@ -7,6 +7,7 @@ from pytmx.util_pygame import load_pygame
 from support import *
 from transition import Transition
 from soil import SoilLayer
+from sky import Rain
 
 class Level:
     def __init__(self) -> None:
@@ -24,6 +25,10 @@ class Level:
         self.setup()
         self.overlay = Overlay(self.player)
         self.transition = Transition(self.reset, self.player)
+        
+        # sky
+        self.rain = Rain(self.all_sprites)
+        self.raining = True
 
     def setup(self) -> None:
         tmx_data = load_pygame('./data/map.tmx')
@@ -102,10 +107,14 @@ class Level:
         self.display_surface.fill('black')
         self.all_sprites.custom_draw(self.player)
         self.all_sprites.update(dt)
-        
 
         self.overlay.display()
         
+        # rain
+        if self.raining:
+            self.rain.update()
+        
+        # transition overlay
         if self.player.sleep:
             self.transition.play()
 
